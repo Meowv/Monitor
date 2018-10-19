7e52f1bdd4cda47350f6ea86f3c82e21c3e3d5ec
阿星Plus <hackxing@foxmail.com>
2018-10-19 18:18:09 +0800

    User Models

---
+++ IntelligentMonitor/Models/Users/Roles.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IntelligentMonitor.Models.Users
+{
+    public class Roles
+    {
+        public int Id { get; set; }
+        public string RoleName { get; set; }
+    }
+}
---
+++ IntelligentMonitor/Models/Users/Users.cs
@@ -0,0 +1,17 @@
+﻿namespace IntelligentMonitor.Models.Users
+{
+    public class Users
+    {
+        public int Id { get; set; }
+
+        public string UserName { get; set; }
+
+        public string NickName { get; set; }
+
+        public string Password { get; set; }
+
+        public int RoleId { get; set; }
+
+        public int IsDelete { get; set; }
+    }
+}
