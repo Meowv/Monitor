a2fa294265d56620d891c8aa85507245e6b09f3b
阿星Plus <hackxing@foxmail.com>
2018-10-21 20:36:39 +0800

    text to md5

---
+++ IntelligentMonitor/Utility/MD5Util.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace IntelligentMonitor.Utility
+{
+    public class MD5Util
+    {
+        public static string TextToMD5(string text)
+        {
+            using (var md5 = MD5.Create())
+            {
+                var bytes = md5.ComputeHash(Encoding.UTF8.GetBytes(text));
+                return BitConverter.ToString(bytes).Replace("-", "");
+            }
+        }
+    }
+}
