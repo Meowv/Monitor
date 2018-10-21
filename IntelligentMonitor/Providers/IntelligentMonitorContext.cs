6d859e596f5f54bebd07b21c3ffd66168628fa3c
阿星Plus <hackxing@foxmail.com>
2018-10-21 17:50:37 +0800

    ef context

---
+++ IntelligentMonitor/Providers/IntelligentMonitorContext.cs
@@ -0,0 +1,21 @@
+﻿using IntelligentMonitor.Models.Users;
+using Microsoft.EntityFrameworkCore;
+
+namespace IntelligentMonitor.Providers
+{
+    public class IntelligentMonitorContext : DbContext
+    {
+        public IntelligentMonitorContext(DbContextOptions<IntelligentMonitorContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Models.Users.Users> Users { get; set; }
+        public DbSet<Roles> Roles { get; set; }
+        public DbSet<Permission> Permission { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
