7cf4f6efb583760e54a03e782d751980ef4c3e03
阿星Plus <hackxing@foxmail.com>
2018-10-21 17:56:17 +0800

    done

--- IntelligentMonitor/Startup.cs
+++ IntelligentMonitor/Startup.cs
@@ -1,7 +1,13 @@
-﻿using IntelligentMonitor.Models.AppSettings;
+﻿using IntelligentMonitor.Authorization;
+using IntelligentMonitor.Models.AppSettings;
+using IntelligentMonitor.Providers;
+using IntelligentMonitor.Providers.Users;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
@@ -24,10 +30,15 @@ namespace IntelligentMonitor
 
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            }).AddCookie();
             services
                 .AddOptions()
                 .Configure<AppSettings>(Configuration)
-                .Configure<ConnectionStrings>(Configuration.GetSection("MySqlConnection"))
+                .Configure<ConnectionStrings>(Configuration.GetSection("ConnectionStrings"))
+                .AddDbContext<IntelligentMonitorContext>(options => options.UseMySql(Configuration.GetConnectionString("MySqlConnection")))
                 .AddSwaggerGen(s =>
                 {
                     s.SwaggerDoc("IntelligentMonitor", null);
@@ -38,6 +49,9 @@ namespace IntelligentMonitor
                     routes.LowercaseUrls = true;
                     routes.AppendTrailingSlash = false;
                 })
+                .AddSingleton<AppSettings>()
+                .AddSingleton<UserProvider>()
+                .AddSingleton<IAuthorizationHandler, PermissionAuthorizationHandler>()
                 .AddMvc()
                 .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
@@ -55,6 +69,8 @@ namespace IntelligentMonitor
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+            app.UseAuthentication();
+
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
