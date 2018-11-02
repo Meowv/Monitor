7f1e5b725a7c76e3d9e633325c06bb1116187bf3
阿星Plus <hackxing@foxmail.com>
2018-11-02 21:12:17 +0800

    date

--- IntelligentMonitor/wwwroot/js/main.js
+++ IntelligentMonitor/wwwroot/js/main.js
@@ -163,9 +163,13 @@ function getTime() {
     var date = new Date();
     var year = date.getFullYear();
     var month = date.getMonth() + 1;
+    month = month < 10 ? '0' + month : month;
     var day = date.getDate();
+    day = day < 10 ? '0' + day : day;
     var hour = date.getHours();
+    hour = hour < 10 ? '0' + hour : hour;
     var minute = date.getMinutes();
+    minute = minute < 10 ? '0' + minute : minute;
     var second = date.getSeconds();
     second = second < 10 ? '0' + second : second;
     var time = year + "-" + month + "-" + day + " " + hour + ":" + minute + ":" + second;
