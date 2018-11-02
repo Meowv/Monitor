e294fa7a6e340d98f0da4d9b2ef45676ce69854a
阿星Plus <hackxing@foxmail.com>
2018-11-02 10:33:01 +0800

    Zabbix Context

---
+++ IntelligentMonitor/Providers/ZabbixContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace IntelligentMonitor.Providers
+{
+    public class ZabbixContext : DbContext
+    {
+        public ZabbixContext(DbContextOptions<ZabbixContext> options) : base(options)
+        {
+        }
+
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
